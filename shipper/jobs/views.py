1157c30f1dc3eae199c4f3e783f0d145d63de1f6
quijano_flores <quijano@braingain.co>
2016-03-05 17:42:58 +0530

    views for json responses with restful api

--- shipper/jobs/views.py
+++ shipper/jobs/views.py
@@ -1,3 +1,120 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from jobs.models import Job
+from jobs.serializers import JobSerializer
+
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def job_list(request):
+    """
+    List all jobs, or create a job.
+    """
+    if request.method == 'GET':
+        queryset = Job.objects.all().order_by('-created_at')
+        serializer = JobSerializer(queryset, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = JobSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def pending_job_list(request):
+    """
+    List all jobs, or create a job.
+    """
+    if request.method == 'GET':
+        queryset = Job.objects.filter(status__in=[0,1]).order_by('-created_at')
+        serializer = JobSerializer(queryset, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = JobSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def active_job_list(request):
+    """
+    List all jobs, or create a job.
+    """
+    if request.method == 'GET':
+        queryset = Job.objects.filter(status__in=[2, 3, 4, 5]).order_by('-created_at')
+        serializer = JobSerializer(queryset, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = JobSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def complete_job_list(request):
+    """
+    List all jobs, or create a job.
+    """
+    if request.method == 'GET':
+        queryset = Job.objects.filter(status__in=[6]).order_by('-created_at')
+        serializer = JobSerializer(queryset, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = JobSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def job_detail(request, pk):
+    """
+    Retrieve, update or delete a job.
+    """
+    try:
+        snippet = Job.objects.get(pk=pk)
+    except Job.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = JobSerializer(snippet)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = JobSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
