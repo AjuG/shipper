270f822934371ca34ac2272affdc39d853df7d44
quijano_flores <quijano@braingain.co>
2016-03-06 11:11:53 +0530

    modified job detail url and addded get available porter url

--- shipper/jobs/urls.py
+++ shipper/jobs/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^pending/$', views.pending_job_list),
     url(r'^active/$', views.active_job_list),
     url(r'^complete/$', views.complete_job_list),
-    url(r'^job/(?P<pk>[0-9]+)/$', views.job_detail),
+    url(r'^detail/(?P<uid>[0-9]+)/$', views.job_detail),
+    url(r'^(?P<job_uid>[0-9]+)/get_porter/(?P<search_radius>[0-9]+)/$', views.get_available_porter),
 ]
